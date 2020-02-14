43beba0aabbfe42e2c1d383b897ae2aa9d77f07f
jonizei <joni.koskinen@cs.tamk.fi>
2020-02-14 18:55:13 +0200

    Button click returns playerDetails and reduces a point

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './Login';
+import Game from './Game';
 import axios from 'axios';
 
 class App extends Component {
+
+    playerInfo = {
+      id: -1,
+      username: '',
+      points: 0
+    }
 
     constructor() {
       super();
@@ -17,6 +24,16 @@ class App extends Component {
 
     }
 
+    componentDidMount() {
+
+      const jwt = sessionStorage.getItem('accessToken');
+
+      if(jwt) {
+        this.setState({loggedIn: true});
+      }
+
+    }
+
     handleLogin(user, pass) {
       console.log("Username: " + user + " Password: " + pass);
       axios.post("http://localhost:8080/login", {
@@ -24,6 +41,7 @@ class App extends Component {
         password: pass
       }).then(res => {
         sessionStorage.setItem('accessToken', res.headers.authorization);
+        this.playerInfo = res.data;
         this.setState({loggedIn: true});
       }).catch(error => {console.log(error)});
     }
@@ -43,10 +61,7 @@ class App extends Component {
       }
 
       return(
-        <div className="App">
-          <p>Hello World!</p>
-          <button onClick={this.handleLogout}>Log out</button>
-        </div>
+        <Game onLogout={this.handleLogout} />
       );
     }
 
--- src/Game.js
+++ src/Game.js
@@ -3,19 +3,33 @@ import axios from 'axios';
 
 class Game extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick = event => {
         event.preventDefault();
+
+        const jwt = sessionStorage.getItem('accessToken');
+
+        axios({
+            method: 'post',
+            url: "http://localhost:8080/api/click/1",
+            headers: {
+                Authorization: jwt
+            }
+        }).then(res => {
+            console.log(res);
+        }).catch(error => {console.log(error)});
+
     }
 
     render() {
         return(
             <div className="Game">
-                <button>Click</button>
+                <button onClick={this.props.onLogout}>Logout</button>
+                <button onClick={this.handleClick}>Click</button>
             </div>
         );
     }
